ad3de9afa79b0441cc5af8aaa8623a1015fa602c
Yhya <yhyamobile57@gmail.com>
2024-04-20 17:50:37 +0200

    seperate the panic handler from main

--- crates/kernel/src/main.rs
+++ crates/kernel/src/main.rs
@@ -1,12 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+mod panic_handler;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
