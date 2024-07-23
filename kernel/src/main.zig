c8c12fe3ee5cf9e3a01b823ff247cb1514f04330
Yhya Ibrahim <yhyamobile57@gmail.com>
2024-07-23 16:39:41 +0300

    emulate a vga text mode on a framebuffer

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -2,13 +2,14 @@ const std = @import("std");
 const limine = @import("limine");
 
 const arch = @import("arch.zig");
+const screen = @import("screen.zig");
+const tty = @import("tty.zig");
 
 pub export var base_revision: limine.BaseRevision = .{ .revision = 2 };
 
 pub export var stack_size_request: limine.StackSizeRequest = .{ .stack_size = 16 * 1024 };
 
 pub export var framebuffer_request: limine.FramebufferRequest = .{};
-var framebuffers: []*limine.Framebuffer = undefined;
 
 export fn _start() callconv(.C) noreturn {
     // Check if limine understands our base revision
@@ -16,12 +17,15 @@ export fn _start() callconv(.C) noreturn {
         arch.hang();
     }
 
-    // Framebuffers is required to complete the initialization
-    if (framebuffer_request.response) |framebuffer_response| {
-        framebuffers = framebuffer_response.framebuffers();
-    } else {
-        arch.hang();
-    }
+    // Initialize the screen before printing
+    screen.init(framebuffer_request.response);
+
+    // Initialize the state of the tty
+    tty.init();
+
+    // Print to test if the printing works
+    tty.print("-- Welcome to The Y Operating System --\n", .{});
+    tty.print("-- Welcome to The Y Operating System ---- Welcome to The Y Operating System ---- Welcome to The Y Operating System ---- Welcome to The Y Operating System ---- Welcome to The Y Operating System ---- Welcome to The Y Operating System ---- Welcome to The Y Operating System ---- Welcome to The Y Operating System --\n", .{});
 
     // The kernel should not return no matter what
     arch.hang();
@@ -30,7 +34,12 @@ export fn _start() callconv(.C) noreturn {
 pub fn panic(message: []const u8, stack_trace: ?*std.builtin.StackTrace, return_address: ?usize) noreturn {
     _ = stack_trace;
     _ = return_address;
-    _ = message;
+
+    // Clear anything on the screen before trying to print
+    tty.clear();
+
+    // TODO: Try to print the stack trace
+    tty.print("kernel panic occured: {s}\n", .{message});
 
     arch.hang();
 }
