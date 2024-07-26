c5e3f5d56dd078789798e05470dc114fb5c88c3f
Yhya Ibrahim <yhyamobile57@gmail.com>
2024-07-26 21:20:38 +0300

    add the missing piece of the idt, more interrupt handlers

--- src/kernel/arch/x86_64/idt.zig
+++ src/kernel/arch/x86_64/idt.zig
@@ -77,9 +77,25 @@ pub fn init() void {
     idt.entries[0].setHandler(@intFromPtr(&handleDivisionError)).setTrapGate();
     idt.entries[1].setHandler(@intFromPtr(&handleDebug)).setTrapGate();
     idt.entries[3].setHandler(@intFromPtr(&handleBreakpoint)).setTrapGate();
+    idt.entries[4].setHandler(@intFromPtr(&handleOverflow)).setTrapGate();
+    idt.entries[5].setHandler(@intFromPtr(&handleBoundRangeExceeded)).setTrapGate();
+    idt.entries[6].setHandler(@intFromPtr(&handleInvalidOpcode)).setTrapGate();
+    idt.entries[7].setHandler(@intFromPtr(&handleDeviceNotAvailable)).setTrapGate();
     idt.entries[8].setHandler(@intFromPtr(&handleDoubleFault)).setTrapGate();
+    idt.entries[10].setHandler(@intFromPtr(&handleSegmentationFault)).setTrapGate();
+    idt.entries[11].setHandler(@intFromPtr(&handleSegmentationFault)).setTrapGate();
+    idt.entries[12].setHandler(@intFromPtr(&handleSegmentationFault)).setTrapGate();
     idt.entries[13].setHandler(@intFromPtr(&handleGeneralProtectionFault)).setTrapGate();
     idt.entries[14].setHandler(@intFromPtr(&handlePageFault)).setTrapGate();
+    idt.entries[16].setHandler(@intFromPtr(&handleX87FloatingPointException)).setTrapGate();
+    idt.entries[17].setHandler(@intFromPtr(&handleAlignmentCheck)).setTrapGate();
+    idt.entries[18].setHandler(@intFromPtr(&handleMachineCheck)).setTrapGate();
+    idt.entries[19].setHandler(@intFromPtr(&handleSIMDFloatingPointException)).setTrapGate();
+    idt.entries[20].setHandler(@intFromPtr(&handleVirtualizationException)).setTrapGate();
+    idt.entries[21].setHandler(@intFromPtr(&handleControlProtectionException)).setTrapGate();
+    idt.entries[28].setHandler(@intFromPtr(&handleHypervisorInjectionException)).setTrapGate();
+    idt.entries[29].setHandler(@intFromPtr(&handleVMMCommunicationException)).setTrapGate();
+    idt.entries[30].setHandler(@intFromPtr(&handleSecurityException)).setTrapGate();
 
     idt.load();
 }
@@ -96,14 +112,71 @@ fn handleBreakpoint(_: *InterruptStackFrame) callconv(.Interrupt) void {
     tty.print("breakpoint\n", .{});
 }
 
+fn handleOverflow(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    tty.print("overflow\n", .{});
+}
+
+fn handleBoundRangeExceeded(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("bound range exceeded\n", .{});
+}
+
+fn handleInvalidOpcode(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("invalid opcode\n", .{});
+}
+
+fn handleDeviceNotAvailable(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("device not available\n", .{});
+}
+
 fn handleDoubleFault(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
     std.debug.panic("double fault: {}\n", .{code});
+}
+
+fn handleSegmentationFault(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
+    std.debug.panic("segmentation fault: {}\n", .{code});
 }
 
 fn handleGeneralProtectionFault(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
     std.debug.panic("general protection fault: {}\n", .{code});
 }
 
+// This should not panic if we made a page allocator
 fn handlePageFault(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
     std.debug.panic("page fault: {}\n", .{code});
+}
+
+fn handleX87FloatingPointException(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("x87 floating point exception\n", .{});
+}
+
+fn handleAlignmentCheck(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
+    std.debug.panic("alignment check: {}\n", .{code});
+}
+
+fn handleMachineCheck(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("machine check\n", .{});
+}
+
+fn handleSIMDFloatingPointException(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("simd floating point exception\n", .{});
+}
+
+fn handleVirtualizationException(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("virtualization exception\n", .{});
+}
+
+fn handleControlProtectionException(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
+    std.debug.panic("control protection exception: {}\n", .{code});
+}
+
+fn handleHypervisorInjectionException(_: *InterruptStackFrame) callconv(.Interrupt) void {
+    std.debug.panic("hypervisor injection exception\n", .{});
+}
+
+fn handleVMMCommunicationException(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
+    std.debug.panic("vmm communication exception: {}\n", .{code});
+}
+
+fn handleSecurityException(_: *InterruptStackFrame, code: u64) callconv(.Interrupt) void {
+    std.debug.panic("security exception: {}\n", .{code});
 }
