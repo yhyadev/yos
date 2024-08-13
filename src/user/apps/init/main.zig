004892a6c16709ca88defb0e74057388201a5500
Yhya Ibrahim <yhyamobile57@gmail.com>
2024-08-13 14:23:27 +0300

    refactor into `init_gui` function

--- src/user/apps/init/main.zig
+++ src/user/apps/init/main.zig
@@ -1,15 +1,19 @@
 const yos = @import("yos");
 
 export fn _start() noreturn {
-    const yui_pid = yos.fork();
+    init_gui();
 
-    if (yui_pid == 0) {
+    while (true) {}
+}
+
+fn init_gui() void {
+    const pid = yos.fork();
+
+    if (pid == 0) {
         const result = yos.execv(&.{"/usr/bin/gui"});
 
         if (result != 0) {
             yos.console.print("could not initialize gui: {}\n", .{result});
         }
     }
-
-    while (true) {}
 }
