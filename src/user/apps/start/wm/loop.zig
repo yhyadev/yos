7024b4b6b4ba1a2af5a8b3d974163be354befaba
Yhya Ibrahim <yhyamobile57@gmail.com>
2024-08-20 18:53:19 +0300

    nocategory: remove another traces of debugging

--- src/user/apps/start/wm/loop.zig
+++ src/user/apps/start/wm/loop.zig
@@ -3,26 +3,13 @@
 //! The event loop that runs all the time, contains key mapping to actions and other functionalities
 
 const abi = @import("abi");
-const core = @import("core");
 
 const display = @import("display.zig");
 
 pub fn start() noreturn {
-    const pid = core.process.fork();
+    while (true) {
+        display.clearBackground(abi.Color.black);
 
-    if (pid == 0) {
-        while (true) {
-            display.synchronize();
-        }
-    } else {
-        const colors: []const abi.Color = &.{ abi.Color.red, abi.Color.blue, abi.Color.green };
-
-        var i: usize = 0;
-
-        while (true) {
-            display.clearBackground(colors[i]);
-
-            if (i < 2) i += 1 else i = 0;
-        }
+        display.synchronize();
     }
 }
